20ad01a14f05bfdf40748dc6c56c42d900f96473
William <capuletos@live.com>
2021-11-02 09:33:23 -0300

    Class fake PurchaseService created

--- src/Service/Purchases.API/Models/PurchaseItem.cs
+++ src/Service/Purchases.API/Models/PurchaseItem.cs
@@ -8,5 +8,7 @@ namespace Purchases.API.Models
         [Required]
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public string Manufacturer { get; set; }
+
     }
 }
---
+++ src/Test/Purchases.Test/PurchaseServiceFake.cs
@@ -0,0 +1,56 @@
+﻿using Purchases.API.Models;
+using Purchases.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Purchases.Test
+{
+    public class PurchaseServiceFake : IPurchaseService
+    {
+
+        private readonly List<PurchaseItem> _purchase;
+
+        public PurchaseServiceFake()
+        {
+            _purchase = new List<PurchaseItem>()
+
+            {
+                new PurchaseItem() {Id= 1, Name = "Teclado Logitech", Manufacturer = "Logtech", Price = 120M},
+                new PurchaseItem() {Id= 2, Name = "Teclado Multilazer", Manufacturer = "Multilazer", Price = 80M},
+                new PurchaseItem() {Id= 3, Name = "Monitor LG 23", Manufacturer = "LG", Price = 120M},
+                new PurchaseItem() {Id= 4, Name = "HD SSD Asus ", Manufacturer = "Assus", Price = 120M}
+            };
+        }
+
+        public IEnumerable<PurchaseItem> GetAllItems()
+        {
+            return _purchase;
+        }
+
+        public PurchaseItem Add(PurchaseItem newItem)
+        {
+            newItem.Id = GetId();
+            _purchase.Add(newItem);
+            return newItem;
+        }
+
+        public PurchaseItem GetById(int id)
+        {
+            return _purchase.Where(a => a.Id == id)
+                .FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            var item = _purchase.First(a => a.Id == id);
+            _purchase.Remove(item);
+        }
+
+        static int GetId()
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+    }
+}
